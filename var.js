2aaefc1d4df41da742e3426b95021ba5436549bb
Sham Gupta <82854568+shamkumar01@users.noreply.github.com>
2023-07-24 11:06:43 +0530

    Add files via upload
    variables
    
    prefer to use let over the var becuse there is the scope issue in the var over the block and function scope .
    for that reason  let is used \

---
+++ var.js
@@ -0,0 +1,16 @@
+const accountId = 144553
+let accountEmail = "sdfh@gmail.com"
+var accountPassword ="12345"
+accountCity ="Jaipur"
+
+let accountState;
+// accountId = 2
+
+
+console.log(accountId);
+
+accountEmail = "sham@sh.com"
+accountPassword ="21212121"
+accountCity = "delhi"
+
+console.table([accountEmail , accountPassword, accountState, accountCity, accountId])
