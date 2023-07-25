ee3c708a946c423748ed01ce521146a8a6b70ea2
Sham Gupta <82854568+shamkumar01@users.noreply.github.com>
2023-07-25 19:36:40 +0530

    datattypes

---
+++ datatypes.js
@@ -0,0 +1,22 @@
+"use strict" // treat all JS code as newer version //
+
+// alert( 3 + 3) we are using nodejs not the browser which has the js engine 
+
+console.log(3
+    +3);
+    console.log("hitesh");
+
+// code readable should be high prioritize //
+
+//datatypes 
+// number , bigint , string , boolean , 
+// null standalone datatype 
+// undefined means there is no  any data is  given
+// symbol mainly it is used in the react for the components(mainly for uniqueness)
+
+
+// object 
+
+let age = 21
+
+console.log(typeof null);  // its type is object and it is an interview question 
