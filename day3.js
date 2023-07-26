7a0f9539bffb67b0461be6d01d21e88edfb6a5a8
Sham Kumar <shamkumar6430@gmail.com>
2023-07-26 21:42:35 +0530

    conversion of the datatypes

---
+++ day3.js
@@ -0,0 +1,34 @@
+// conversion of the datatypes to check all the data
+
+
+
+// let score = undefined
+
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number (score)
+// console.log(typeof valueInNumber);
+
+
+// console.log(valueInNumber);
+
+// "33 => 33"
+// "33abc"  => NaN  not a number 
+// string it gives nan
+
+
+// let isLoggedIn = "sham"
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+// console.log(typeof isLoggedIn );
+// console.log(booleanIsLoggedIn);
+
+// 1=> true if empty it gives false and if string is written then it gives the true 
+
+let someNumber = 33 
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
