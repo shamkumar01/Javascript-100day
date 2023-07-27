dbf71d0f25e81d6bfd0f11ed7b297bcd5950eef5
Sham Kumar <shamkumar6430@gmail.com>
2023-07-27 19:46:00 +0530

    mdn documenttation and the comparison

--- day4.js
+++ day4.js
@@ -10,7 +10,17 @@ let str2 = "sham"
 
 let str3 = str1+str2
 
-console.log(str3);
+// console.log(str3);
 
 
-console.log(1 + "2");
+// console.log(1 + "2");
+        
+// console.log((3+ 4 )* 5 % 3);
+
+// console.log(+"");
+
+// let num1 , num2 , num3
+
+// num1 =num2 = num3 = 2+2
+
+// learn  about the prefix and postfix increment and decrement on the mdn for the documentatuion for the detail knowledge
